8bdbb568017f284d46689ecaa030a19a90a519f2
Ali Sartaz Khan <71156712+alisartazkhan@users.noreply.github.com>
2022-10-01 22:28:41 -0700

    Update HandTracker.py with comments

--- HandTracker.py
+++ HandTracker.py
@@ -1,9 +1,18 @@
+'''
+Name: Ali Sartaz Khan
+Description:Track the left hand of an individual using the front facing camera and correctly identify 
+any number from 1-10 represented using Sign Language. 
+'''
+
 import os
 import cv2
 import math
 import mediapipe as mp
 
 class handDetector():
+    '''
+    Class that uses the front camera and detects all the 22 points in the person's finger.
+    '''
     def __init__(self, mode=False, maxHands=1, modelComplex = 1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
@@ -14,11 +23,14 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-
     def findHands(self, img, draw=True):
+        '''
+        Functions detects the hand and draws dots on all 22 points of the hand.
+        
+        img: One frame obtained from the front camera of the laptop
+        '''
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
@@ -26,21 +38,25 @@ class handDetector():
                                                self.mpHands.HAND_CONNECTIONS)
         return img
     def findPosition(self, img, handNo=0, draw=True):
+        '''
+        Find position of fingers and gives their coordinates
+        
+        img: 1 frame obtained from the front camera of the laptop
+        '''
         lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                # print(id, cx, cy)
                 lmList.append([id, cx, cy])
-                # if draw:
-                #     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
         return lmList
 
 
 def imageList():
+    '''
+    Function returns a list of images is the FingerImages directory.
+    '''
     folderPath = "FingerImages"
     myList = os.listdir(folderPath)
     overlayList = []
@@ -50,6 +66,65 @@ def imageList():
         overlayList.append(image)
 
     return overlayList
+
+
+def checkFingers(fingers, img, overlayList, lmList):
+    '''
+    Checks if the fingers are in a particular orientation that matches the ASL representations 1-10.
+    If so, it displays the appropriate number image representated by the person.
+    
+    fingers: List of 0s and 1s for 5 fingers starting from the thumb to the pinky where 1 means finger is up and 0
+             means finger is down
+    img: One frame obtained from front camera
+    overlayList: List containing all the pictures of ASL gestures 1-10
+    lmList: 2D list of all fingers and their coordinates
+    '''
+    if fingers == [0, 1, 0, 0, 0]: # one
+        displayImage(img, overlayList[0])
+    elif fingers == [0, 1, 1, 0, 0]: #two
+        displayImage(img, overlayList[1])
+    elif fingers == [1, 1, 1, 0, 0]: #three
+        displayImage(img, overlayList[2])
+    elif fingers == [0, 1, 1, 1, 1]: #four
+        displayImage(img, overlayList[3])
+    elif fingers == [1, 1, 1, 1, 1]: #five
+        displayImage(img, overlayList[4])
+    elif fingers == [0, 1, 1, 1, 0] and isTouching(lmList, 20, 4): #six
+        displayImage(img, overlayList[5])
+    elif fingers == [0, 1, 1, 0, 1] and isTouching(lmList, 16, 4): #seven
+        displayImage(img, overlayList[6])
+    elif fingers == [1, 1, 0, 1, 1] and isTouching(lmList, 12, 4): #eight
+        displayImage(img, overlayList[7])
+    elif fingers == [1, 0, 1, 1, 1] and isTouching(lmList, 8, 4): #nine
+        displayImage(img, overlayList[8])
+    elif fingers == [1, 0, 0, 0, 0]: #ten
+        displayImage(img, overlayList[9])
+
+def displayImage(img, image):
+    '''
+    Displays the Image
+    img: One frame obtained from the front camera
+    image: Image to be displayed
+    '''
+    height, width, channel = image.shape
+    img[0:height, 0:width] = image
+
+
+
+def isTouching(lmList, finger1, finger2):
+    '''
+    Function checks if the two fingers in the parameters are touching and returns true or false.
+    
+    lmList: 2D list of all fingers and their coordinates
+    finger1: ID number of the finger
+    finger2: ID number of the finger
+    '''
+    x1, y1 = lmList[finger1][1], lmList[finger1][2]
+    x2, y2 = lmList[finger2][1], lmList[finger2][2]
+    length = math.hypot(x2 - x1, y2 - y1)
+    if length < 50:
+        return True
+    return False
 
 def main():
     wCap, hCap = 640, 480
@@ -81,46 +156,4 @@ def main():
         cv2.imshow("Image", img)
         cv2.waitKey(1)
 
-def checkFingers(fingers, img, overlayList, lmList):
-    '''
-    Works great with the left hand
-    :param fingers:
-    :param img:
-    :param overlayList:
-    :return:
-    '''
-    if fingers == [0, 1, 0, 0, 0]: # one
-        displayImage(img, overlayList[0])
-    elif fingers == [0, 1, 1, 0, 0]: #two
-        displayImage(img, overlayList[1])
-    elif fingers == [1, 1, 1, 0, 0]: #three
-        displayImage(img, overlayList[2])
-    elif fingers == [0, 1, 1, 1, 1]: #four
-        displayImage(img, overlayList[3])
-    elif fingers == [1, 1, 1, 1, 1]: #five
-        displayImage(img, overlayList[4])
-    elif fingers == [0, 1, 1, 1, 0] and isTouching(lmList, 20, 4): #six
-        displayImage(img, overlayList[5])
-    elif fingers == [0, 1, 1, 0, 1] and isTouching(lmList, 16, 4): #seven
-        displayImage(img, overlayList[6])
-    elif fingers == [1, 1, 0, 1, 1] and isTouching(lmList, 12, 4): #eight
-        displayImage(img, overlayList[7])
-    elif fingers == [1, 0, 1, 1, 1] and isTouching(lmList, 8, 4): #nine
-        displayImage(img, overlayList[8])
-    elif fingers == [1, 0, 0, 0, 0]: #ten
-        displayImage(img, overlayList[9])
-
-def displayImage(img, image):
-    height, width, channel = image.shape
-    img[0:height, 0:width] = image
-
-
-
-def isTouching(lmList, finger1, finger2):
-    x1, y1 = lmList[finger1][1], lmList[finger1][2]
-    x2, y2 = lmList[finger2][1], lmList[finger2][2]
-    length = math.hypot(x2 - x1, y2 - y1)
-    if length < 50:
-        return True
-    return False
 main()
